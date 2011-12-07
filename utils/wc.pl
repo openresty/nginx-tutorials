eab4d4514080e416030cdbf46566c4a4d74db632
agentzh (章亦春) <agentzh@gmail.com>
2011-12-07 21:22:35 +0800

    updated wc.pl to output word count in the perspective of a publisher.

--- utils/wc.pl
+++ utils/wc.pl
@@ -4,23 +4,45 @@ use strict;
 use warnings;
 use encoding 'utf8';
 
-my $infile = shift or
+if (!@ARGV) {
     die "No input file specified.\n";
-
-open my $in, "<:encoding(UTF-8)", $infile
-or
-    die "cannot open $infile for reading: $!\n";
-
-my $hanc = 0;
-my $c = 0;
-
-while (<$in>) {
-    $hanc++ while /\p{Han}/g;
-    $c += length($_);
 }
 
-close $in;
+my $hanc = 0;
+my $wc = 0;
+my $c = 0;
+
+for my $infile (@ARGV) {
+    open my $in, "<:encoding(UTF-8)", $infile
+    or
+        die "cannot open $infile for reading: $!\n";
+
+    while (<$in>) {
+        $hanc++ while /\p{Han}/g;
+        while (1) {
+            if (/\G\s+/gc) {
+                next;
+            } elsif (/\G(?:\p{Han}|[”“，；：？。！…]|[,;.])/gc) {
+                $wc++;
+                next;
+
+            } elsif (/\G[A-Za-z0-9_]+/gc) {
+                $wc++;
+                next;
+            } elsif (/\G./gc) {
+                next;
+            }
+
+            last;
+        }
+
+        $c += length($_);
+    }
+
+    close $in;
+}
 
 print "$hanc han chars found.\n";
+print "$wc words found.\n";
 print "$c chars found.\n";
 
