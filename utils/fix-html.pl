acafaf3bd58dec1bd7ccfd9dc5c640dbbcc4e537
agentzh (章亦春) <agentzh@gmail.com>
2011-12-09 14:38:45 +0800

    added more links to variable tutorial 02 and 03.

--- utils/fix-html.pl
+++ utils/fix-html.pl
@@ -10,6 +10,7 @@ my $src = <>;
 $src =~ s{(<pre.*?)(>.*?<)(/pre>)}{$1.fmt($2).$3}ges;
 $src =~ s!</p><p>!</p><br><p>&nbsp; &nbsp; !gs;
 $src =~ s!^\s*<p>!<p>&nbsp; &nbsp; !g;
+$src =~ s!^\s*(\p{Han})!<p>&nbsp; &nbsp; $1!s;
 $src =~ s!href="/!href="http://wiki.nginx.org/!g;
 $src =~ s!unescape!\&\#117;nescape!gs;
 print $src;
