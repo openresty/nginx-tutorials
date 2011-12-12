fd671498d190d151a41d78964f765541725371fe
agentzh (章亦春) <agentzh@gmail.com>
2011-12-12 23:21:27 +0800

    checked in a missing file.

---
+++ utils/gen-html-index.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use encoding 'utf8';
+use strict;
+use warnings;
+
+use Getopt::Std;
+
+my %opts;
+getopts('o:', \%opts) or usage();
+
+my $outfile = $opts{o};
+
+my @nums = qw(
+   零 一 二 三 四 五 六 七 八 九
+   十 十一 十二 十三 十四 十五 十六 十七 十八 十九
+   二十
+);
+
+my @infiles = @ARGV;
+
+my $res = <<_EOC_;
+<html>
+    <head>
+        <title>agentzh 的 Nginx 教程</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+    <h3>目录</h3>
+_EOC_
+
+$res .= "<ul>\n";
+for my $infile (@ARGV) {
+    if ($infile =~ /NginxVariables(\d+)/) {
+        my $num = +$1;
+        my $n = $nums[$num];
+        #$infile =~ s{.*/}{}g;
+        $res .= <<_EOC_;
+    <li><a href="$infile">Nginx 变量漫谈（$n）</a></li>
+_EOC_
+    } else {
+        die "Unknown file $infile";
+    }
+}
+
+$res .= "</ul></body></html>";
+
+if ($outfile) {
+    open my $out, ">:encoding(UTF-8)", $outfile
+        or die "Cannot open $outfile for writing: $!\n";
+
+    print $out $res;
+    close $out;
+
+} else {
+    print $res;
+}
+
+sub usage {
+    die "Usage: $0 [-o <outfile>] <infile>\n";
+}
+
