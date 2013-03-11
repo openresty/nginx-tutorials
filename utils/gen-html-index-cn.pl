0dea7cae5b9094ac41b751b2ca6392746558716c
agentzh (Yichun Zhang) <agentzh@gmail.com>
2013-03-10 21:53:52 -0700

    bugfix: when the user cannot access google at all (and hangs), the google analytics tracking code should not block the whole page from displaying.

--- utils/gen-html-index-cn.pl
+++ utils/gen-html-index-cn.pl
@@ -29,13 +29,15 @@ my $res = <<_EOC_;
 
           var _gaq = _gaq || [];
           _gaq.push(['_setAccount', 'UA-24724965-1']);
+          _gaq.push(['_setDomainName', 'openresty.org']);
           _gaq.push(['_trackPageview']);
 
-          (function() {
+          var _gaf = (function() {
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+          });
+          setTimeout(_gaf, 0);
 
         </script>
     </head>
