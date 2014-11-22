3bf4cc4cf98cd7faee817e816174961bf1c7687e
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-11-21 20:23:34 -0800

    fixed the non-HTML formats.

--- ebooks.mk
+++ ebooks.mk
@@ -23,29 +23,38 @@ pdf: $(name).pdf
 html: $(name).html
 
 %.pdf: $(name).html
+	cp tutorial-simple.css tutorial.css
 	ebook-convert $< $@ \
-	    --margin-bottom 30 \
-	    --margin-top 30 \
+	    --margin-bottom 25 \
+	    --margin-top 25 \
 	    --margin-left 20 \
 	    --margin-right 20 \
 	    --no-chapters-in-toc \
 	    --book-producer 'agentzh' \
+	    --pdf-default-font-size 1 \
+	    --pdf-mono-font-size 1 \
+	    --paper-size a4 \
 	    --output-profile kindle \
 	    --title "$(title)" --publisher 'agentzh' \
 	    --language $(lang2) --authors 'agentzh'
+	git co tutorial.css
 
 %.mobi: $(name).html
+	cp tutorial-simple.css tutorial.css
 	ebook-convert $< $@ \
 	    --output-profile kindle_dx --no-inline-toc \
 	    --title "$(title)" --publisher 'agentzh' \
 	    --language $(lang2) --authors 'agentzh'
+	git co tutorial.css
 
 %.epub: $(name).html
+	cp tutorial-simple.css tutorial.css
 	ebook-convert $< $@ \
 	    --no-default-epub-cover \
 	    --output-profile kindle_dx \
 	    --title "$(title)" --publisher 'agentzh' \
 	    --language $(lang2) --authors 'agentzh'
+	git co tutorial.css
 
 $(name).html: $(htmlfiles) ./utils/gen-html-index-$(lang2).pl
 	./utils/gen-html-index-$(lang2).pl -v $(ver) -o $@ $(htmlfiles)
